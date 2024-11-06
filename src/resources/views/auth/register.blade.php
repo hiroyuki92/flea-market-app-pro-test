dafb7cb898d9042735ef8181e3a98e82571101ff
yukiko <yukiko@magnolia.co.jp>
2024-11-06 14:11:53 +0900

    会員登録画面作成

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,5 +1,63 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/auth/register.css')}}">
+<link rel="stylesheet" href="{{ asset('css/register.css')}}">
 @endsection
+
+@section('content')
+<div class="register-form__content">
+    <div class="register-form__heading">
+        <h2>会員登録</h2>
+    </div>
+    <form class="form">
+        @csrf
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">ユーザー名</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input class="form__input" type="text" name="name" value="{{ old('name') }}" />
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">メールアドレス</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input class="form__input" type="email" name="email" value="{{ old('email') }}" />
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">パスワード</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input class="form__input"  type="password" name="password" value="{{ old('password') }}" />
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">確認用パスワード</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input class="form__input" type="password" name="password_confirmation" value="{{ old('password_confirmation') }}" />
+                </div>
+            </div>
+        </div>
+        <div class="form__group--btn">
+            <button type="submit" class="register-btn">登録する</button>
+        </div>
+    </form>
+    <div class="form__group--login">
+        <button type="submit" class="login-btn">ログインはこちら</button>
+    </div>
+</div>
+
+@endsection('content')
