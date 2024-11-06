8c4a8cfbb8982dc1f59060d50e6b13b2dfef682f
yukiko <yukiko@magnolia.co.jp>
2024-11-06 11:23:31 +0900

    add:cssファイル作成、レイアウトファイル作成

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coachtech</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+        @yield('css')
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+</head>
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <img src="{{ asset('images/logo.svg') }}" alt="サイトのロゴ">
+            @yield('link')
+        </div>
+    </header>
+    <div class="content">
+        @yield('content')
+    </div>
+</body>
+</html>
