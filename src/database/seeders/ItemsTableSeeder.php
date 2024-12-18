3633d1e219e0280bb09b81f98e1a07701025c361
yukiko <yukiko@magnolia.co.jp>
2024-12-18 13:30:44 +0900

    fix:ItemsTableSeederにてuser_idをランダムに変更

--- src/database/seeders/ItemsTableSeeder.php
+++ src/database/seeders/ItemsTableSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Item;
+use App\Models\User;
 use Illuminate\Support\Facades\Storage;
+use Faker\Generator as Faker;
 
 class ItemsTableSeeder extends Seeder
 {
@@ -15,9 +17,10 @@ class ItemsTableSeeder extends Seeder
      */
     public function run()
 {
+    $userIds = User::pluck('id')->toArray();
+    $faker = app(Faker::class);
     $items = [
         [
-            'user_id' => 1,
             'name' => '腕時計',
             'brand' => 'アルマーニ',
             'price' => 15000,
@@ -27,7 +30,6 @@ class ItemsTableSeeder extends Seeder
             'categories' => [1, 5]
         ],
         [
-            'user_id' => 2,
             'name' => 'HDD',
             'brand' => '富士通',
             'price' => 5000,
@@ -37,7 +39,6 @@ class ItemsTableSeeder extends Seeder
             'categories' => [2]
         ],
         [
-            'user_id' => 3,
             'name' => '玉ねぎ3束',
             'brand' => '淡路島',
             'price' => 300,
@@ -47,7 +48,6 @@ class ItemsTableSeeder extends Seeder
             'categories' => [10]
         ],
         [
-            'user_id' => 4,
             'name' => '革靴',
             'brand' => 'ポールスミス',
             'price' => 4000,
@@ -57,7 +57,6 @@ class ItemsTableSeeder extends Seeder
             'categories' => [1, 5]
         ],
         [
-            'user_id' => 5,
             'name' => 'ノートPC',
             'brand' => 'Mac',
             'price' => 45000,
@@ -67,7 +66,6 @@ class ItemsTableSeeder extends Seeder
             'categories' => [2]
         ],
         [
-            'user_id' => 6,
             'name' => 'マイク',
             'brand' => '東芝',
             'price' => 8000,
@@ -77,7 +75,6 @@ class ItemsTableSeeder extends Seeder
             'categories' => [2]
         ],
         [
-            'user_id' => 7,
             'name' => 'ショルダーバッグ',
             'brand' => 'COACH',
             'price' => 3500,
@@ -87,7 +84,6 @@ class ItemsTableSeeder extends Seeder
             'categories' => [1, 4]
         ],
         [
-            'user_id' => 8,
             'name' => 'タンブラー',
             'brand' => 'スターバックス',
             'price' => 500,
@@ -97,7 +93,6 @@ class ItemsTableSeeder extends Seeder
             'categories' => [10]
         ],
         [
-            'user_id' => 9,
             'name' => 'コーヒーミル',
             'brand' => 'ネスカフェ',
             'price' => 4000,
@@ -107,7 +102,6 @@ class ItemsTableSeeder extends Seeder
             'categories' => [10]
         ],
         [
-            'user_id' => 1,
             'name' => 'メイクセット',
             'brand' => 'KOSE',
             'price' => 2500,
@@ -119,8 +113,9 @@ class ItemsTableSeeder extends Seeder
     ];
 
     foreach ($items as $itemData) {
+        $randomUserId = $faker->randomElement($userIds);
         $item = Item::create([
-            'user_id' => $itemData['user_id'],
+            'user_id' => $randomUserId,
             'name' => $itemData['name'],
             'brand' => $itemData['brand'],
             'price' => $itemData['price'],
