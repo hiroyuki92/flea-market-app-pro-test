92055fd59931a9c68baf38d750742feb9ba7937d
yukiko <yukiko@magnolia.co.jp>
2025-04-25 11:24:09 +0900

    メール認証画面デザイン修正

--- src/resources/views/auth/verify.blade.php
+++ src/resources/views/auth/verify.blade.php
@@ -1,31 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<main class="container center">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">メールアドレスの確認</div>
-
-                <div class="card-body">
+            <div class="card" style="border: none;">
+                <div class="card-body d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
                     @if (session('message'))
                         <div class="alert alert-success" role="alert">
                             {{ session('message') }}
                         </div>
                     @endif
 
-                    メールアドレスの確認が必要です。
-                    確認用メールをご確認ください。
+                    <p style="font-weight: bold;">登録していただいたメールアドレスに認証メールを送付しました。</p>
+                    <p style="font-weight: bold;">メール認証を完了してください。</p>
 
                     <form method="POST" action="{{ route('verification.send') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">
-                            確認メールを再送信する
+                        <button type="submit" class="btn btn-link p-0 m-0" style="text-decoration: none;">
+                            認証メールを再送する
                         </button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</main>
 @endsection
