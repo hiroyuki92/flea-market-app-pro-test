736f2bb848bdb231ad77b4b75b5865bef412b660
yukiko <yukiko@magnolia.co.jp>
2025-04-28 09:23:05 +0900

    デザイン修正edit.css

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Coachtech</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
         @yield('css')
     <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
@@ -12,7 +13,9 @@
 <body>
     <header class="header">
         <div class="header__inner">
-            <img src="{{ asset('images/logo.svg') }}" alt="サイトのロゴ">
+            <a href="{{ url('/') }}">
+                <img src="{{ asset('images/logo.svg') }}" alt="サイトのロゴ">
+            </a>
             @yield('link')
         </div>
     </header>
